568cd51d00f9c0f82d13faee19e6b17edae722eb
Pekka Heikkinen <pekka.h.heikkinen@gmail.com>
2014-05-10 21:34:57 +0300

    Update VertexGroupsFromRigify.py

--- VertexGroupsFromRigify.py
+++ VertexGroupsFromRigify.py
@@ -1,8 +1,15 @@
+"""
+Adds every DEFORMING bone as a vertex group
+"""
+
 import bpy
 
 for mesh in bpy.context.selected_objects:
+    bpy.data.objects[mesh.name].vertex_groups.clear()
     for obj in bpy.context.scene.objects:
         if obj.type == 'ARMATURE':
             for b in obj.pose.bones:
                 if b.name.startswith("DEF"):
-                    bpy.data.objects[mesh.name].vertex_groups.new(b.name)
+                    vg = bpy.data.objects[mesh.name].vertex_groups.new(b.name)
+                    for f in bpy.data.objects[mesh.name].data.polygons:
+                        vg.add(f.vertices, 0.0, 'ADD')
