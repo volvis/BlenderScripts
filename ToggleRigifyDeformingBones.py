8b422b9f3e4f7389ed6fb79982880fb8fa8b7ac2
Pekka Heikkinen <pekka.h.heikkinen@gmail.com>
2014-05-11 10:27:04 +0300

    Create ToggleRigifyDeformingBones.py

---
+++ ToggleRigifyDeformingBones.py
@@ -0,0 +1,43 @@
+"""
+Toggles the skeleton layers in Rigify rigs between
+a) Displaying the deformer bones for skinning
+b) Displaying the controller objects for posing
+"""
+
+import bpy
+
+class VToggleRigifyDeformingBones(bpy.types.Operator):
+    bl_idname = "ops.toggle_deforming_bones"
+    bl_label = "Toggle Deforming Bones (Rigify)"
+    
+    def execute(self, context):
+        control_layers = [0,2,4,6,12,15,24]
+        for armature in bpy.data.armatures:
+            if 'rig_id' in armature:
+                controls = armature.layers[29]
+                for layer in control_layers:
+                    armature.layers[layer] = controls
+                armature.layers[29] = not controls
+        return {'FINISHED'}
+
+class VSkinningPanel(bpy.types.Panel):
+    bl_label = "Skinning"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.operator("ops.toggle_deforming_bones")
+
+
+def register():
+    bpy.utils.register_module(__name__)
+
+
+def unregister():
+    bpy.utils.register_module(__name__)
+
+
+if __name__ == "__main__":
+    register()
