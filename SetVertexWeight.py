354ff3215e9f8fad3d08c70ec7ba576c86da1407
Pekka Heikkinen <pekka.h.heikkinen@gmail.com>
2014-05-11 13:38:59 +0300

    Update SetVertexWeight.py

--- SetVertexWeight.py
+++ SetVertexWeight.py
@@ -1,3 +1,8 @@
+"""
+Tool shortcuts for applying weights to vertices similar to 3dsMax.
+Select bone, vertex and 
+"""
+
 import bpy
 
 def set_weight(weight = 1.0):
@@ -20,4 +25,59 @@ def set_weight(weight = 1.0):
     bpy.ops.paint.weight_paint_toggle()
     bpy.ops.paint.weight_paint_toggle()
 
-set_weight(0)
+
+class VSetVertexWeight(bpy.types.Operator):
+    bl_idname = "ops.set_vertex_weight"
+    bl_label = "Set Vertex Weight"
+    
+    weight = bpy.props.FloatProperty(name="Weight", default=1.0, min=0.0, max=1.0)
+    
+    def execute(self, context):
+        set_weight(weight=self.weight)
+        return {'FINISHED'}
+
+
+class VSetVertexWeightPanel(bpy.types.Panel):
+    bl_label = "Vertex Weight"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_context = "weightpaint"
+    
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row(align=True)
+        row.alignment = 'EXPAND'
+        row.operator("ops.set_vertex_weight", text="0.0").weight = 0.0
+        row.operator("ops.set_vertex_weight", text="0.5").weight = 0.5
+        row.operator("ops.set_vertex_weight", text="1.0").weight = 1.0
+        
+        row = layout.row(align=True)
+        row.alignment = 'EXPAND'
+        row.operator("ops.set_vertex_weight", text="0.25").weight = 0.25
+        row.operator("ops.set_vertex_weight", text="0.75").weight = 0.75
+        
+        row = layout.row(align=True)
+        row.alignment = 'EXPAND'
+        row.operator("ops.set_vertex_weight", text="0.33").weight = 0.33333
+        row.operator("ops.set_vertex_weight", text="0.66").weight = 0.66666
+        
+        row = layout.row()
+        row.operator("ops.toggle_deforming_bones")
+        
+        row = layout.row()
+        row.operator("bpy.context.object.data.use_paint_mask")
+        
+        row = layout.row()
+        row.prop(bpy.context.object.data, "use_paint_mask_vertex"
+        row.prop(bpy.context.object.data, "use_paint_mask")
+
+def register():
+    bpy.utils.register_module(__name__)
+
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
+
+if __name__ == "__main__":
+    register()
