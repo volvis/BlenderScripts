01444474ae85f332d7285f6c5c89b4893ef631c6
Pekka Heikkinen <pekka.h.heikkinen@gmail.com>
2014-05-11 15:10:51 +0300

    Update and rename SetVertexWeight.py to VertexWeightTools.py

--- VertexWeightTools.py
+++ VertexWeightTools.py
@@ -1,11 +1,27 @@
 """
 Tool shortcuts for applying weights to vertices similar to 3dsMax.
-Select bone, vertex and 
+Select bone, vertices and click the desired value
 """
 
 import bpy
 
-def set_weight(weight = 1.0):
+
+class VToggleRigifyDeformingBones(bpy.types.Operator):
+    bl_idname = "aijai.toggle_deforming_bones"
+    bl_label = "Toggle Deforming Bones (Rigify)"
+    
+    def execute(self, context):
+        control_layers = [0,2,4,6,12,15,24]
+        for armature in bpy.data.armatures:
+            if 'rig_id' in armature:
+                controls = armature.layers[29]
+                for layer in control_layers:
+                    armature.layers[layer] = controls
+                armature.layers[29] = not controls 
+        return {'FINISHED'}
+
+
+def aijai_set_weight(weight = 1.0):
     pose_bone = bpy.context.active_pose_bone
     wp_object = bpy.context.weight_paint_object
 
@@ -27,13 +43,14 @@ def set_weight(weight = 1.0):
 
 
 class VSetVertexWeight(bpy.types.Operator):
-    bl_idname = "ops.set_vertex_weight"
+    bl_idname = "aijai.set_vertex_weight"
     bl_label = "Set Vertex Weight"
+    bl_options = {'REGISTER', 'UNDO'}
     
-    weight = bpy.props.FloatProperty(name="Weight", default=1.0, min=0.0, max=1.0)
+    weight = bpy.props.FloatProperty(name="Vertex Weight", default=1.0, min=0.0, max=1.0)
     
     def execute(self, context):
-        set_weight(weight=self.weight)
+        aijai_set_weight(weight=self.weight)
         return {'FINISHED'}
 
 
@@ -45,31 +62,36 @@ class VSetVertexWeightPanel(bpy.types.Panel):
     
     def draw(self, context):
         layout = self.layout
-        row = layout.row(align=True)
-        row.alignment = 'EXPAND'
-        row.operator("ops.set_vertex_weight", text="0.0").weight = 0.0
-        row.operator("ops.set_vertex_weight", text="0.5").weight = 0.5
-        row.operator("ops.set_vertex_weight", text="1.0").weight = 1.0
         
         row = layout.row(align=True)
         row.alignment = 'EXPAND'
-        row.operator("ops.set_vertex_weight", text="0.25").weight = 0.25
-        row.operator("ops.set_vertex_weight", text="0.75").weight = 0.75
+        row.operator("aijai.set_vertex_weight", text="0.0").weight = 0.0
+        row.operator("aijai.set_vertex_weight", text="0.5").weight = 0.5
+        row.operator("aijai.set_vertex_weight", text="1.0").weight = 1.0
         
         row = layout.row(align=True)
         row.alignment = 'EXPAND'
-        row.operator("ops.set_vertex_weight", text="0.33").weight = 0.33333
-        row.operator("ops.set_vertex_weight", text="0.66").weight = 0.66666
+        row.operator("aijai.set_vertex_weight", text="0.25").weight = 0.25
+        row.operator("aijai.set_vertex_weight", text="0.75").weight = 0.75
+        
+        row = layout.row(align=True)
+        row.alignment = 'EXPAND'
+        row.operator("aijai.set_vertex_weight", text="0.33").weight = 0.33333
+        row.operator("aijai.set_vertex_weight", text="0.66").weight = 0.66666
         
         row = layout.row()
-        row.operator("ops.toggle_deforming_bones")
+        row.operator("aijai.toggle_deforming_bones")
         
         row = layout.row()
-        row.operator("bpy.context.object.data.use_paint_mask")
+        row.operator("aijai.context.object.data.use_paint_mask")
         
         row = layout.row()
-        row.prop(bpy.context.object.data, "use_paint_mask_vertex"
+        row.prop(bpy.context.object.data, "use_paint_mask_vertex")
         row.prop(bpy.context.object.data, "use_paint_mask")
+        
+        row = layout.row()
+        row.operator("aijai.set_vertex_weight", "Type Vertex Weight")
+
 
 def register():
     bpy.utils.register_module(__name__)
@@ -81,3 +103,4 @@ def unregister():
 
 if __name__ == "__main__":
     register()
+
